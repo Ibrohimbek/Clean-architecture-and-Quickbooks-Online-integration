a45a6759dd707225a4a87f5df43807bb81711d0b
Ibrohim Ermatov <ibrohimbek@gmail.com>
2017-06-17 04:58:29 +0500

    Set up database

--- qb_online/qb_online/settings.py
+++ qb_online/qb_online/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zph=2u7+6s333dd^4fptb2ce9zqt_1k+&2^h&+0klyz2$++-@b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'qb_online.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qb_online',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
